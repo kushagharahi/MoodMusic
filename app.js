61f1a7780b52596c3509df634f8679583d3d7fec
Kusha Gharahi <kusha95@gmail.com>
2016-11-19 01:49:04 -0600

    got basic hello world to tone analyzer running

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var api = require('genius-api');
-var genius = new api('GNOcS9Axns37xmJCZqgCg9-c5F3PgMUb_AjVIz7X0KUT7vCk7ZL7PWfurr6hGhic');
+var ToneAnalyzerV3 = require("watson-developer-cloud/tone-analyzer/v3");
 
-genius.song(378195).then(function(response) {
-  console.log('song', response.song);  
+var tone_analyzer = new ToneAnalyzerV3({
+  url: "https://gateway.watsonplatform.net/tone-analyzer/api",
+  password: "kHNhWiuLgPES",
+  username: "ccc2e192-5aed-4834-ae90-8af66cbf2b32",
+  version_date: "2016-05-19"
+});
+
+var text = "I let it fall, my heart, And as it fell you rose to claim it It was dark and I was over Until you kissed my lips and you saved me My hands, theyre strong But my knees were far too weak";
+
+  tone_analyzer.tone({ text: text },
+  function(err, tone) {
+    if (err)
+      console.log(err);
+    else {
+      //console.log(tone);
+      console.log(tone["document_tone"]["tone_categories"][0]["tones"][0]["score"]); //anger
+      console.log(tone["document_tone"]["tone_categories"][0]["tones"][1]["score"]); //fear
+      console.log(tone["document_tone"]["tone_categories"][0]["tones"][2]["score"]); //disgust
+      console.log(tone["document_tone"]["tone_categories"][0]["tones"][3]["score"]); //joy
+      console.log(tone["document_tone"]["tone_categories"][0]["tones"][4]["score"]); //sadness
+      //console.log(JSON.stringify(tone, null, 2));
+     }
 });
